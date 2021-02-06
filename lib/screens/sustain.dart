b13c93ae8f9dddab1ee258bf2c33a46d7ea4442b
enesbiyyik <enes.biyyik@outlook.com>
2021-02-06 12:45:13 +0300

    güzel oldu burası da düşünmemiz gereken şeyler var

--- lib/screens/sustain.dart
+++ lib/screens/sustain.dart
@@ -2,16 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:sustainable_tracker/components/sustainGridItem.dart';
 
 class SustainScreen extends StatelessWidget {
-
   SustainScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: 3/4), 
-      itemBuilder: (context, index){
-        return SustainGridItem(); 
-      },
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      color: Colors.blue,
+      child: Stack(
+        children: [
+          Positioned(
+            top: 50,
+            child: Text("ANAN DA ÖYLE DERDİ")
+          ),
+          Positioned.fill(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height/2,
+                    color: Colors.transparent,
+                  ),
+                  Container(       
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height/2,
+                    color: Colors.green,
+                  ),
+                  Container(       
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height/2,
+                    color: Colors.red,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
